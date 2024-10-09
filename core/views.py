68d980cfc6115673692e7e690b43e3871e96921e
Vanclides Melo <dadaldantas@hotmail.com>
2024-10-09 19:21:39 -0300

    projeto finalizado

---
+++ core/views.py
@@ -0,0 +1,42 @@
+from .models import Livro, Categoria, Autor
+from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
+from rest_framework import generics
+from .filters import LivroFilter
+
+
+class LivroList(generics.ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    filterset_class = LivroFilter
+    search_fields = ("^titulo",)
+    ordering_fields = ["titulo", "autor", "categoria", "publicado_em"]
+    name = "livro-list"
+    
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
+
+class AutorList(generics.ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-list"
+    
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
+    
+class CategoriaList(generics.ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+    search_fields = ("^name",)
+    ordering_fields = ["name"]
+    name = "categotia-list"
+  
+
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
